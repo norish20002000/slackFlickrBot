807c5d102bab124ea3f04353eab1e708e7ba209d
shuro <norish20002000@gmail.com>
2018-09-07 16:01:16 +0900

    改行削除

--- flickrBot.py
+++ flickrBot.py
@@ -39,7 +39,7 @@ def getFlickrImage(keyword, num = 1):
     if not res["photos"]["photo"]:
         resultStr = u"こめんなさい。:woman-facepalming:画像の取得に失敗しちゃいました。\n何度か試してみてください。:wink:"
     else:
-        resultStr = u"こちらの画像で、いかがかしら:wink:" + "\n" + res["photos"]["photo"][0][urlKind]
+        resultStr = u"こちらの画像で、いかがかしら:wink:" + res["photos"]["photo"][0][urlKind]
     
     pprint.pprint(resultStr)
 
